cf83337e079ea07640a8573e6576c14bff21aa39
Dominik Slapa <dominik.slapa@gmail.com>
2022-02-02 15:21:59 +0100

    Image zoom

--- components/ProjectImage/ProjectImage.jsx
+++ components/ProjectImage/ProjectImage.jsx
@@ -1,5 +1,9 @@
 import { Image } from "@chakra-ui/react";
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
 
 export const ProjectImage = ({ src, alt }) => (
-  <Image borderRadius="lg" w="full" src={src} alt={alt} mb={4} />
+  <Zoom>
+    <Image borderRadius="lg" w="full" src={src} alt={alt} mb={4} />
+  </Zoom>
 );
