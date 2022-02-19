a234e378007f0c94949121c028b46580b3976cd6
Dominik Slapa <dominik.slapa@gmail.com>
2022-02-19 11:33:06 +0100

    Google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,27 @@ import { theme } from "../styles/theme";
 import "@fontsource/m-plus-rounded-1c";
 import { GetServerSidePropsContext } from "next";
 import Fonts from "../lib/Fonts";
+import { useEffect } from "react";
+import * as ga from "../lib/ga";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ChakraProvider theme={theme}>
       <Fonts />
